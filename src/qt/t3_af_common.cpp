6c38b6c54bcce490e90353a8ac79cff0dba31bac
ccmorning <31011315@qq.com>
2018-01-22 18:31:03 +0800

    update

--- src/qt/t3_af_common.cpp
+++ src/qt/t3_af_common.cpp
@@ -9,9 +9,16 @@ T3_AF_common::T3_AF_common(QWidget *parent) :
     ui->setupUi(this);
     this->setGeometry(0, 0, _father->_width_, _father->_height_);
     this->setWindowFlags(Qt::Window|Qt::FramelessWindowHint|Qt::WindowStaysOnBottomHint);
+    //this->setWindowFlags(Qt::Window|Qt::FramelessWindowHint);
     this->showFullScreen();
     //
     T3LOG("0+");
+}
+
+void T3_AF_common::paintEvent(QPaintEvent *)
+{
+    QPainter paint_(this);
+    paint_.drawPixmap(0, 0, this->width(), this->height(), QPixmap(":/Pictures/background_background.jpeg"));
 }
 
 T3_AF_common::~T3_AF_common()
